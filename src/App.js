4ce29628b8e054da3f6498e53cc9d8ba9b5435da
Tumble <15913224+tumble1999@users.noreply.github.com>
2021-10-11 15:06:46 +0100

    jekyll and react test

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export function App() {
+
+
+	return (
+		<div id="dashboard" class="modules">
+			<div id="server">
+				<img src="/assets/img/tumbot.svg" alt="tumblot_logo" />
+				Tumbot
+				<i>V</i>
+			</div>
+			<div id="nav">
+				<a href="#">Modules</a>
+				<a href="#">Users</a>
+				<img src="/assets/img/tumbot.svg" alt="tumblot_logo" />
+			</div>
+			<div id="modules">
+				<a href="#">Module 1</a>
+				<a href="#">Module 2</a>
+				<a href="#">Module 3</a>
+				<a href="#">Module 4</a>
+				<a href="#">Module 5</a>
+			</div>
+			<div id="main">
+				<div className="panel page-header">
+					<h1>Page Title</h1>
+					<a href="#" class="btn">JSON</a>
+					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium rerum adipisci necessitatibus accusamus repudiandae nostrum voluptates asperiores veritatis et quas, odit sunt dolores aliquid fugiat atque, optio perferendis totam illum?</p>
+				</div>
+
+				<div className="panel">
+					hmm
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path'), MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	output: {
+		path: path.join(__dirname, "assets"),
+		filename: "js/tumbot.min.js"
+	},
+	devServer: {
+		port: 4000
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader'
+				]
+			}
+		]
+	},
+	plugins: [new MiniCssExtractPlugin({ filename: "css/tumbot.min.css" })]
+};
