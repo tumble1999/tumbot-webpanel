f876fc0546689abaa449cbebc758ff517fa477b1
Tumble <tumblegamer@gmail.com>
2021-12-07 12:42:32 +0000

    use oauth

--- src/App.js
+++ src/App.js
@@ -1,42 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ModuleList, ServerList, ViewList } from "./Lists";
 import { ModuleEditor, PermissionEditor } from "./Editor.js";
 import { getParams } from './params';
-import { setupConnection } from './bot';
+import { setupConnection, socket, botUrl } from './bot';
 
 function App() {
-	let {server,module,users,stage}= getParams({server,module,users,stage});
-	if(!stage) stage = "unstable";
-	switch(stage) {
-		case "sandbox":
-			setupConnection("http://localhost:3000")
-		break;
-		case "unstable":
-			setupConnection("https://tumbot-unstable.herokuapp.com")
-		break;
-		case "stable":
-			setupConnection("https://tumbot-stable.herokuapp.com")
-		break;
+	let { server, module, users, stage, code } = getParams({ server, module, users, stage, code });
+	if (!stage) stage = "unstable";
+
+	if (socket) {
+		if (code) {
+			localStorage.setItem('tumbot-' + stage, code);
+
+		} else {
+			code = localStorage.getItem('tumbot-' + stage);
+			if (!code) {
+				location.href = botUrl + "/auth/discord";
+			}
+		}
+	} else {
+		switch (stage) {
+			case "sandbox":
+				setupConnection("http://localhost:3000");
+				break;
+			case "unstable":
+				setupConnection("https://tumbot-unstable.herokuapp.com");
+				break;
+			case "stable":
+				setupConnection("https://tumbot-stable.herokuapp.com");
+				break;
+		}
+	}
+	if (code) {
+		useEffect(() => {
+			socket.emit("login", {code});
+		}, [socket]);
 	}
 
-	if(!server)server = "all";
-	if(!module)module = "core";
 
-	console.log({server,module,users});
+	if (!server) server = "all";
+	if (!module) module = "core";
 
-	let view,viewId = void 0 == users?"modules":"users";
+	console.log({ server, module, users });
 
-	if(void 0 == users) {
+	let view, viewId = void 0 == users ? "modules" : "users";
+
+	if (void 0 == users) {
 		view = [
 			<ModuleList stage={stage} key="module-list" serverId={server} moduleId={module} />,
 			<ModuleEditor stage={stage} key="module-editor" serverId={server} moduleId={module} />
-		]
+		];
 	} else {
-		view = <PermissionEditor stage={stage} serverId={server} />
+		//view = <PermissionEditor stage={stage} serverId={server} />;
 	}
 
 	return <div>
-		<ServerList stage={stage} serverId={server}/>
+		<ServerList stage={stage} serverId={server} />
 		{/* <ViewList  stage={stage} serverId={server} viewId={viewId} /> */}
 		{view}
 	</div>;
--- src/bot.js
+++ src/bot.js
@@ -1,15 +1,16 @@
 import { io } from 'socket.io-client';
-let socket;
+let socket,botUrl;
 
-export function setupConnection(ip) {
-	socket = io(ip);
+export function setupConnection(url) {
+	botUrl = url
+	socket = io(url);
 
 	socket.on("connect", () => {
-		console.log("Connected to " + ip);
+		console.log("Connected to " + url);
 	});
 
 	socket.on("log", console.log);
 }
 
 
-export { socket };
+export { socket,botUrl};
